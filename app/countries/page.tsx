5589523544aa230a310a194ba1e249838c122fd4
AndyOoh <andy.oeee@gmail.com>
2024-01-23 22:31:33 +0700

    services

--- app/countries/page.tsx
+++ app/countries/page.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import { get } from 'http';
+import React from 'react';
 
-type Props = {}
+type Props = {};
 
-export default function CountriesPage({}: Props) {
+export default async function CountriesPage({}: Props) {
+  const countries = await getCountries();
+
   return (
     <div>
-        <h1>Countries Page</h1>
+      <h1>Countries Page</h1>
     </div>
-  )
+  );
 }
